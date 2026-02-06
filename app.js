6eb691dfc54c34843c72581ab0fa4c94bf677f80
paulcalver <paul@paulcalver.cc>
2026-02-06 00:45:17 +0000

    error fix for render deploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const port = process.env.PORT || 3000;
+
 
 app.use(express.static('public'));
 
